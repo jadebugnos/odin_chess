71269b66112d5054005ee242226a298a387f64c5
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-09 08:54:41 +0800

    feat: add game board displaying logic
    - update board specs to remove useless specs

--- lib/board.rb
+++ lib/board.rb
@@ -1,14 +1,21 @@
 # this file holds the ChessBoard class which holds all game board logic
 class ChessBoard
   def initialize
-    @board = nil
+    @board = Array.new(8) { Array.new(8, ' ') }
   end
 
-  def create_board
-    if @board.nil?
-      @board = Array.new(8) { Array.new(8, '|  |') }
-    else
-      @board
+  # holds the board displaying logic
+  def display_board
+    horizontal = "  +#{'---+' * 8}"
+    i = 8
+    @board.each do |row|
+      puts horizontal
+      puts "#{i} |" + row.map { |cell| "#{cell.center(3)}|" }.join
+      i -= 1
     end
+    puts horizontal
+    puts '    ' + ('a'..'h').map { |c| c.center(4) }.join # rubocop:disable Style/StringConcatenation
   end
+
+  def update_board; end
 end
