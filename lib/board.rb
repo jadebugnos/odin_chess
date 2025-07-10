99f1b97b297811231971f00c53bff15b1e7b57d3
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-10 08:11:49 +0800

    feat: implement dynamic setup of initial piece positions

--- lib/board.rb
+++ lib/board.rb
@@ -1,7 +1,10 @@
+require_relative 'pieces'
+
 # this file holds the ChessBoard class which holds all game board logic
 class ChessBoard
   def initialize
-    @board = Array.new(8) { Array.new(8, ' ') }
+    @board = Array.new(8) { Array.new(8, '') }
+    @chess_pieces = ChessPiece.new
   end
 
   # holds the board displaying logic
@@ -10,11 +13,29 @@ class ChessBoard
     i = 8
     @board.each do |row|
       puts horizontal
-      puts "#{i} |" + row.map { |cell| "#{cell.center(3)}|" }.join
+      puts "#{i} |" + row.map { |cell| "#{cell.to_s.center(3)}|" }.join
       i -= 1
     end
     puts horizontal
     puts '    ' + ('a'..'h').map { |c| c.center(4) }.join # rubocop:disable Style/StringConcatenation
+  end
+
+  def add_pieces(board, key, positions)
+    positions.each do |x, y|
+      board[x][y] = key
+    end
+  end
+
+  def set_up_pieces
+    # adding white pieces
+    @chess_pieces.positions[:white].each do |unicode, coordinates|
+      add_pieces(@board, unicode, coordinates)
+    end
+
+    # adding black pieces
+    @chess_pieces.positions[:black].each do |unicode, coordinates|
+      add_pieces(@board, unicode, coordinates)
+    end
   end
 
   def update_board; end
