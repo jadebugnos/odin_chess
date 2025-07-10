f5d5b3526617f18d3a50014744e567f3d3b6d35a
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-10 09:31:11 +0800

    test: add #set_up_pieces method test samples

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,10 +1,43 @@
 require_relative '../lib/board'
 
 RSpec.describe ChessBoard do
-  describe '#display_board' do
-    context 'when called' do
-      it 'will display a board' do
-      end
+  describe '#set_up_pieces' do
+    subject(:chess_board) { described_class.new }
+    let(:chess_pieces) do
+      double('chess_pieces', positions: {
+               white: {
+                 "\u2656" => [[7, 0], [7, 7]],                     # ♖ rook
+                 "\u2658" => [[7, 1], [7, 6]],                     # ♘ knight
+                 "\u2657" => [[7, 2], [7, 5]],                     # ♗ bishop
+                 "\u2655" => [[7, 3]],                             # ♕ queen
+                 "\u2654" => [[7, 4]],                             # ♔ king
+                 "\u2659" => (0..7).map { |col| [6, col] }         # ♙ pawn
+               },
+               black: {
+                 "\u265C" => [[0, 0], [0, 7]],                     # ♜ rook
+                 "\u265E" => [[0, 1], [0, 6]],                     # ♞ knight
+                 "\u265D" => [[0, 2], [0, 5]],                     # ♝ bishop
+                 "\u265B" => [[0, 3]],                             # ♛ queen
+                 "\u265A" => [[0, 4]],                             # ♚ king
+                 "\u265F" => (0..7).map { |col| [1, col] }         # ♟ pawn
+               }
+             })
+    end
+
+    before do
+      array = Array.new(8) { Array.new(8, '') }
+      chess_board.instance_variable_set(:@board, array)
+      chess_board.instance_variable_set(:@chess_pieces, chess_pieces)
+      allow(chess_board).to receive(:add_pieces)
+    end
+    it 'will send a message to Chessboard to return initial positions' do
+      expect(chess_pieces).to receive(:positions)
+      chess_board.set_up_pieces
+    end
+
+    it 'will call #add_pieces exactly 12 times to add white and black pieces' do
+      expect(chess_board).to receive(:add_pieces).exactly(12).times
+      chess_board.set_up_pieces
     end
   end
 
