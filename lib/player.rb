fd392ffcbc4570a56c93725c706e758ad9bbc544
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-11 22:35:28 +0800

    feat: add helper methods for modularity

--- lib/player.rb
+++ lib/player.rb
@@ -2,6 +2,7 @@ class Player # rubocop:disable Style/Documentation
   def validate_player_move
     move = []
     loop do
+      print 'Enter your move (e.g., 2b): '
       input = gets.chomp
 
       move << input if check_input?(input) # helper method to check input validity
@@ -11,11 +12,32 @@ class Player # rubocop:disable Style/Documentation
     move
   end
 
-  # def check_input?(input)
-  #   num, letter = input.split('')
+  def check_input?(input)
+    unless input.length == 2
+      puts 'only accepts 1 number between 1-8 and a letter a-h'
+      return false
+    end
 
-  #   if (Integer(num)).between?(1, 8)
-  #     puts ''
-  #   end
-  # end
+    num = input[0]
+    letter = input[1]
+
+    check_num(num) && check_letter(letter)
+  end
+
+  def check_num(num)
+    return true if ('1'..'8').include?(num)
+
+    puts 'invalid input! Enter a valid number'
+    false
+  end
+
+  def check_letter(str)
+    return true if ('a'..'h').include?(str)
+
+    puts 'invalid input! Enter a valid letter'
+    false
+  end
 end
+
+player = Player.new
+p player.validate_player_move
