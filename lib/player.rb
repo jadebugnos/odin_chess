4553b1ede4c1f803e3f0eeba30784f4d836dc61c
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-12 09:53:58 +0800

    feat: add Player#print_move_stage method

--- lib/player.rb
+++ lib/player.rb
@@ -2,14 +2,22 @@ class Player # rubocop:disable Style/Documentation
   def validate_player_move
     move = []
     loop do
-      print 'Enter your move (e.g., 2b): '
-      input = gets.chomp
+      print_move_stage(move.length)
+      input = gets.chomp.strip.downcase
 
       move << input if check_input?(input) # helper method to check input validity
       break if move.length == 2
     end
 
     move
+  end
+
+  def print_move_stage(move)
+    if move.zero?
+      puts 'Select a piece to move: '
+    else
+      puts 'select a cell to move: '
+    end
   end
 
   def check_input?(input)
@@ -21,23 +29,14 @@ class Player # rubocop:disable Style/Documentation
     num = input[0]
     letter = input[1]
 
-    check_num(num) && check_letter(letter)
+    check_num_and_letter?(num, letter)
   end
 
-  def check_num(num)
-    return true if ('1'..'8').include?(num)
+  def check_num_and_letter?(num, str)
+    return true if ('1'..'8').include?(num) &&
+                   ('a'..'h').include?(str)
 
-    puts 'invalid input! Enter a valid number'
-    false
-  end
-
-  def check_letter(str)
-    return true if ('a'..'h').include?(str)
-
-    puts 'invalid input! Enter a valid letter'
+    puts 'Invalid input! Try Again'
     false
   end
 end
-
-player = Player.new
-p player.validate_player_move
