978548b0e7ec7be7e142b21742101d570a4a3969
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-12 09:52:24 +0800

    test: add specs for Player class methods

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -10,23 +10,22 @@ RSpec.describe Player do
         second = '4a'
         allow(player_move).to receive(:gets).and_return(first, second)
         allow(player_move).to receive(:check_input?).and_return(true, true)
+        allow(player_move).to receive(:print_move_stage)
       end
 
-      it 'it will return an array with 2 elements' do
-        player_input = player_move.validate_player_move.length
+      let(:player_input) { player_move.validate_player_move }
 
-        expect(player_input).to eql(2)
+      it 'it will return an array with 2 elements' do
+        result = %w[1a 4a]
+
+        expect(player_input).to eql(result)
       end
 
       it 'will contain letters between a to h' do
-        player_input = player_move.validate_player_move
-
         expect(player_input).to all(match(/[a-h]/))
       end
 
       it 'will have numbers between 1-8' do
-        player_input = player_move.validate_player_move
-
         expect(player_input).to all(match(/\A[1-8]/))
       end
     end
@@ -39,6 +38,7 @@ RSpec.describe Player do
         second_valid = '4a'
         allow(player_move).to receive(:gets).and_return(first_invalid, second_invalid, first_valid, second_valid)
         allow(player_move).to receive(:check_input?).and_return(false, false, true, true)
+        allow(player_move).to receive(:print_move_stage)
       end
 
       it 'will keep asking the player input until input is valid' do
@@ -48,7 +48,86 @@ RSpec.describe Player do
     end
   end
 
-  # describe '#check_input?' do
-  #   subject(:)
-  # end
+  describe '#check_input?' do
+    subject(:player_input) { described_class.new }
+
+    context 'when the input length is not exactly 2' do
+      before do
+        allow(player_input).to receive(:check_num_and_letter?)
+        allow(player_input).to receive(:puts)
+      end
+      it 'will return false' do
+        input = '11a'
+        length_is_not_two = player_input.check_input?(input)
+
+        expect(length_is_not_two).to eq(false)
+      end
+    end
+
+    context 'when the input length is exactly 2' do
+      it 'will continue the execution and call check_num_and_letter' do
+        input = '1a'
+
+        expect(player_input).to receive(:check_num_and_letter?).with('1', 'a')
+        player_input.check_input?(input)
+      end
+
+      it 'will return true if input pass the validation' do
+        allow(player_input).to receive(:check_num_and_letter?).and_return(true)
+        input = '3g'
+        valid_input = player_input.check_input?(input)
+
+        expect(valid_input).to eq(true)
+      end
+    end
+  end
+
+  describe '#check_num_and_letter?' do
+    context 'when the num and letter arguments are valid' do
+      subject(:valid_player_input) { described_class.new }
+
+      it 'will return true' do
+        num = '8'
+        letter = 'h'
+        valid_arguments = valid_player_input.check_num_and_letter?(num, letter)
+
+        expect(valid_arguments).to eq(true)
+      end
+    end
+
+    context 'when the num argument is invalid' do
+      subject(:invalid_arguments) { described_class.new }
+      it 'will return false if non numeric character' do
+        num = '#'
+        letter = 'h'
+        none_num_arg = invalid_arguments.check_num_and_letter?(num, letter)
+
+        expect(none_num_arg).to eq(false)
+      end
+
+      it 'will return false if num is out of range' do
+        num = '23'
+        letter = 'c'
+        num_out_of_range = invalid_arguments.check_num_and_letter?(num, letter)
+
+        expect(num_out_of_range).to eq(false)
+      end
+
+      it 'will return false if letter is non letter' do
+        num = '4'
+        letter = '$'
+        non_letter_arg = invalid_arguments.check_num_and_letter?(num, letter)
+
+        expect(non_letter_arg).to eq(false)
+      end
+
+      it 'will return false if letter is out of rage (a-h)' do
+        num = '5'
+        letter = 'k'
+        out_of_range_char = invalid_arguments.check_num_and_letter?(num, letter)
+
+        expect(out_of_range_char).to eq(false)
+      end
+    end
+  end
 end
