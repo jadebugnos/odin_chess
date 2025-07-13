78d255ca2576be0a043fd5a5b019df4c782dfeda
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-13 08:04:23 +0800

    instantiate objects to start the game for debugging

--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
 require_relative 'lib/board'
+require_relative 'lib/game'
+require_relative 'lib/player'
 
-ChessBoard.new
+player = Player.new
+board = ChessBoard.new
+new_game = ChessGame.new(player, board)
+new_game.play_game
