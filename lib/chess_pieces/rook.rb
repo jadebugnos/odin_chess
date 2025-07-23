bc6dc28f21b0fa68993c31db5e19fda562596e59
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-23 09:00:32 +0800

    feat: define classes for each chess pieces

---
+++ lib/chess_pieces/rook.rb
@@ -0,0 +1,17 @@
+require_relative '../pieces'
+
+class Rook < ChessPiece
+  def initialize # rubocop:disable Lint/MissingSuper
+    @unicode = ["\u2656", "\u265C"] # [white, black]
+    @legal_directions = [
+      [-1, 0],  # up
+      [1, 0],   # down
+      [0, -1],  # left
+      [0, 1]    # right
+    ]
+  end
+
+  def legal_move?(color, move)
+    
+  end
+end
