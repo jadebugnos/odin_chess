841585c1454281eb0f8bf151b9e46720cd7b062d
Jade Bugnos <173978908+jadebugnos@users.noreply.github.com>
2025-07-25 07:09:09 +0800

    refactor: move legal_move? logic from Rook to ChessPiece
    - Centralize movement logic in ChessPiece to reduce duplication
    - Update ChessPiece specs to cover legal_move? behavior

--- lib/pieces.rb
+++ lib/pieces.rb
@@ -1,25 +1,27 @@
 # this file defines the Pieces class which holds the chess pieces logic
 class ChessPiece
-  def initialize(uni = nil, lwm = nil, lbm = nil)
+  FIXED_DELTA_PIECES = ['♘', '♞', '♔', '♚'].freeze
+
+  def initialize(uni = nil, lgm = nil, lgd = nil)
     @unicode = uni
-    @legal_moves_white = lwm
-    @legal_moves_black = lbm
+    @legal_moves = lgm
+    @legal_directions = lgd
   end
 
-  # returns true if the players move is legal otherwise false.
-  # Parameters:
-  # - color: Symbol representing the player's color (:white or :black).
-  #          Used to select the appropriate legal move set.
-  # - move: An array of two coordinates [from, to], where each is a [row, col] pair.
-  def legal_move?(color, move)
-    legal_moves = color == :white ? @legal_moves_white : @legal_moves_black
-    from = move[0]
-    to = move[1]
+  def legal_move?(move, board, *_args)
+    from_x, from_y = move[0]
+    to_x, to_y     = move[1]
 
-    # Calculate delta (to - from)
-    delta_move = [to[0] - from[0], to[1] - from[1]]
+    return matches_pattern?(move) if FIXED_DELTA_PIECES.include?(board[from_x][from_y])
 
-    legal_moves.include?(delta_move)
+    delta = handle_deltas(from_x, to_x, from_y, to_y)
+
+    return false unless @legal_directions.include?(delta)
+
+    start = move[0]
+    destination = move[1]
+
+    valid_path?(start, destination, delta, board)
   end
 
   # moves the piece to a target cell
@@ -31,5 +33,54 @@ class ChessPiece
   def move_piece(board, current, target)
     board[current[0]][current[1]] = ''
     board[target[0]][target[1]] = @unicode
+  end
+
+  private
+
+  def get_delta(from, to)
+    if from < to
+      1
+    elsif from > to
+      -1
+    else
+      0
+    end
+  end
+
+  def handle_deltas(from_x, to_x, from_y, to_y)
+    delta_x = get_delta(from_x, to_x)
+    delta_y = get_delta(from_y, to_y)
+    [delta_x, delta_y]
+  end
+
+  def valid_path?(start, destination, direction, board)
+    current_x = start[0] + direction[0]
+    current_y = start[1] + direction[1]
+
+    while current_x.between?(0, 7) && current_y.between?(0, 7)
+      return true if destination == [current_x, current_y]
+
+      return false unless board[current_x][current_y] == ''
+
+      current_x += direction[0]
+      current_y += direction[1]
+    end
+
+    false
+  end
+
+  # returns true if the players move is legal otherwise false.
+  # Parameters:
+  # - color: Symbol representing the player's color (:white or :black).
+  #          Used to select the appropriate legal move set.
+  # - move: An array of two coordinates [from, to], where each is a [row, col] pair.
+  def matches_pattern?(move)
+    from = move[0]
+    to = move[1]
+
+    # Calculate delta (to - from)
+    delta_move = [to[0] - from[0], to[1] - from[1]]
+
+    @legal_moves.include?(delta_move)
   end
 end
--- spec/chess_piece_spec.rb
+++ spec/chess_piece_spec.rb
@@ -2,43 +2,58 @@ require_relative '../lib/pieces'
 
 RSpec.describe ChessPiece do
   describe '#legal_move?' do
-    let(:legal_white_moves) { [[-1, 0], [-1, -1], [-1, 1]] }
-    let(:legal_black_moves) { [[1, 0], [1, -1], [1, 1]] }
-    subject(:player_move) { described_class.new(nil, legal_white_moves, legal_black_moves) }
+    let(:empty_row) { Array.new(8, '') }
+    let(:empty_board) { Array.new(8) { empty_row.dup } }
 
-    context 'when the move is legal' do
-      it 'will return true' do
-        color = :white
-        move = [[4, 2], [3, 2]]
-        legal_move = player_move.legal_move?(color, move)
-
-        expect(legal_move).to eq(true)
+    context 'when piece uses fixed delta (e.g., knight)' do
+      let(:knight_unicode) { '♘' }
+      let(:knight_moves) do
+        [[-2, -1], [-2, 1], [-1, -2], [-1, 2],
+         [1, -2], [1, 2], [2, -1], [2, 1]]
       end
 
-      it 'will return false if the move is illegal and the color is black' do
-        color = :black
-        move = [[4, 6], [5, 7]]
-        legal_move = player_move.legal_move?(color, move)
+      let(:knight) { ChessPiece.new(knight_unicode, knight_moves) }
 
-        expect(legal_move).to eq(true)
+      it 'returns true for a valid knight move' do
+        board = Marshal.load(Marshal.dump(empty_board))
+        board[4][4] = knight_unicode
+        move = [[4, 4], [2, 5]] # delta = [-2, 1]
+        expect(knight.legal_move?(move, board)).to be true
+      end
+
+      it 'returns false for an invalid knight move' do
+        board = Marshal.load(Marshal.dump(empty_board))
+        board[4][4] = knight_unicode
+        move = [[4, 4], [3, 4]] # delta = [-1, 0]
+        expect(knight.legal_move?(move, board)).to be false
       end
     end
 
-    context 'when the move is illegal' do
-      it 'will return false' do
-        color = :white
-        move = [[4, 2], [5, 4]]
-        illegal_move = player_move.legal_move?(color, move)
+    context 'when piece uses directions (e.g., rook)' do
+      let(:rook_unicode) { '♖' }
+      let(:rook_directions) { [[-1, 0], [1, 0], [0, -1], [0, 1]] }
+      let(:rook) { ChessPiece.new(rook_unicode, nil, rook_directions) }
 
-        expect(illegal_move).to eq(false)
+      it 'returns true for an unobstructed vertical move' do
+        board = Marshal.load(Marshal.dump(empty_board))
+        board[0][0] = rook_unicode
+        move = [[0, 0], [3, 0]] # delta = [1, 0]
+        expect(rook.legal_move?(move, board)).to be true
       end
 
-      it 'will return false if the color is black and the move is illegal' do
-        color = :black
-        move = [[4, 6], [6, 7]]
-        legal_move = player_move.legal_move?(color, move)
+      it 'returns false for an obstructed vertical move' do
+        board = Marshal.load(Marshal.dump(empty_board))
+        board[0][0] = rook_unicode
+        board[2][0] = '♙'
+        move = [[0, 0], [3, 0]] # blocked by pawn at [2, 0]
+        expect(rook.legal_move?(move, board)).to be false
+      end
 
-        expect(legal_move).to eq(false)
+      it 'returns false for a diagonal move' do
+        board = Marshal.load(Marshal.dump(empty_board))
+        board[0][0] = rook_unicode
+        move = [[0, 0], [1, 1]] # delta = [1, 1]
+        expect(rook.legal_move?(move, board)).to be false
       end
     end
   end
